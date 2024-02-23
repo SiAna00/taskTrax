291dbc0efce1a971888cf14ecaaf2795bec9f5db
anasi <asimicev@gmail.com>
2024-02-23 16:39:34 +0100

    change home.html, create header.html, create flask app and route for idex.html

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ import os
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text, ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+Bootstrap5(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
