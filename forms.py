68b5a22ab9224dcb6bfb847649a29c39033bcbc7
anasi <asimicev@gmail.com>
2024-02-24 13:51:41 +0100

    add login.html, add.html

--- forms.py
+++ forms.py
@@ -3,8 +3,7 @@ from wtforms import DateField, StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 class RegisterForm(FlaskForm):
-    first_name = StringField("First Name", validators=[DataRequired()])
-    last_name = StringField("Last Name", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email Address", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
     register_button = SubmitField("Create Account")
@@ -17,7 +16,6 @@ class LoginForm(FlaskForm):
 class AddTaskForm(FlaskForm):
     task = StringField("Task", validators=[DataRequired()])
     description = StringField("Description", validators=[DataRequired()])
-    created_date = DateField("Created", format="%d-%m-%Y")
-    due_date = DateField("Due", format="%d-%m-%Y")
+    due_date = DateField("Due")
     status = StringField("Status", validators=[DataRequired()])
     add_task_button = SubmitField("Add Task")
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 import os
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text, ForeignKey
+from sqlalchemy import DateTime, Integer, String, Text, ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import AddTaskForm, LoginForm, RegisterForm
 
@@ -37,12 +38,21 @@ class Task(db.Model):
     author = relationship("User", back_populates="tasks")
     task: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     description: Mapped[str] = mapped_column(String(250), nullable=False)
-    created_date: Mapped[str] = mapped_column(String(250), nullable=False)
-    due_date: Mapped[str] = mapped_column(String(250), nullable=False)
+    created_date: Mapped[date] = mapped_column(DateTime(), nullable=False)
+    due_date: Mapped[date] = mapped_column(DateTime(), nullable=False)
     status: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
 
 with app.app_context():
     db.create_all()
+
+
+# Configure Flask login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 @app.route("/")
@@ -51,8 +61,10 @@ def home():
 
 
 @app.route("/tasks")
+@login_required
 def show_tasks():
-    return render_template("tasks.html")
+    tasks = db.session.execute(db.select(Task)).scalars().all()
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -60,8 +72,28 @@ def register():
     register_form = RegisterForm()
 
     if register_form.validate_on_submit():
-        pass
+        name = register_form.name.data
+        email = register_form.email.data
+        password = generate_password_hash(register_form.password.data, method="pbkdf2:sha256:600000", salt_length=16)
 
+        user = db.session.execute(db.select(User).where(User.name == name)).scalar()
+
+        if user:
+            return redirect("/login")
+        
+        new_user = User(
+            name=name,
+            email=email,
+            password=password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user(user)
+
+        return redirect("/tasks")
+        
     return render_template("register.html", form=register_form)
 
 
@@ -70,9 +102,61 @@ def login():
     login_form = LoginForm()
 
     if login_form.validate_on_submit():
-        pass
+        email = login_form.email.data
+        password = login_form.password.data
+
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user)
+                return redirect("/tasks")
+            
+            flash("Incorrect password. Please try again.")
+            return render_template("login.html", form=login_form)
+        
+        return redirect("/register")
 
     return render_template("login.html", form=login_form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route("/add", methods=["GET", "POST"])
+@login_required
+def add_task():
+    add_form = AddTaskForm()
+
+    if add_form.validate_on_submit():
+        task = add_form.task.data
+        description = add_form.description.data
+        created_date = date.today()
+        due_date = add_form.due_date.data
+        status = add_form.status.data
+
+        new_task = Task(
+            author=current_user,
+            task=task,
+            description=description,
+            created_date=created_date,
+            due_date=due_date,
+            status=status
+        )
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        flash("Task added successfully.")
+        return redirect("/tasks")
+    
+    return render_template("add.html", form=add_form)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
